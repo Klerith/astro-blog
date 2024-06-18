9f514f0ebc6d60404a3fc6a696a41bf423ad0a2c
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-18 12:43:03 -0400

    chore: Update npm dependencies
    Update npm dependencies to the latest stable versions of markdown-it and sanitize-html.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,11 @@ import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
+
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blogPosts = await getCollection('blog');
 
@@ -11,16 +16,33 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     title: 'Fernando Blog',
     // `<description>` field in output xml
     description: 'Un simple blog sobre mis aventura con Astro',
+
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
+
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: site ?? '',
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: blogPosts.map(({ data, slug }) => ({
+    items: blogPosts.map(({ data, slug, body }) => ({
       title: data.title,
       pubDate: data.date,
       description: data.description,
       link: `/posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+
+      customData: `<media:content
+          type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+          width="${data.image.width}"
+          height="${data.image.height}"
+          medium="image"
+          url="${site + data.image.src}" />
+      `,
     })),
     // (optional) inject custom xml
     customData: `<language>es-mx</language>`,
